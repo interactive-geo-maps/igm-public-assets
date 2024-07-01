1bd4f1b12b8820293dd61d857ae5e9e2226773c6
MapGeo CI <dev@interactivegeomaps.com>
2024-07-01 09:06:18 +0000

    CI Release Version 1.6.18

--- vendor/glightbox/js/glightbox.js
+++ vendor/glightbox/js/glightbox.js
@@ -1,5 +1,4 @@
 (function (global, factory) {
-  console.log('test v1.0.1');
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.GLightbox = factory());
